b304aca8a52c7e12bf8248d3f2d5dcda301473f2
kks1234 <945366712@qq.com>
2019-02-06 23:45:34 +0800

    two

--- rtthread/3.0.3/libcpu/arm/cortex-m3/cpuport.c
+++ rtthread/3.0.3/libcpu/arm/cortex-m3/cpuport.c
@@ -47,10 +47,33 @@ rt_uint8_t *rt_hw_stack_init(void       *tentry,
 	stk = stack_addr + sizeof(rt_uint32_t) ;
 	
 	/* 让 stk 向下8字节对齐 */
+	stk = (rt_uint8_t *)RT_ALIGN_DOWN((rt_uint32_t)stk,8);
 	
-
+	/* stk 指针继续向下移动sizeof(struct stack_frame)个偏移 */   /* ??? */
+	stk -= sizeof(struct stack_frame);
 	
+	/* 将stk强制转化成stack_frame类型后存到stack_frame */
+	stack_frame = (struct stack_frame*)stk;
 	
+	/* 以stack_frame 为起始地址,占空间里面的sizeof(struct stack_frame)个内存
+	 初始化为 0xdeadbeef*/
+	for(i = 0 ; i < sizeof(struct stack_frame)/sizeof(rt_uint32_t) ; i ++)
+	{
+		((rt_uint32_t *)stack_frame)[i] = 0xdeadbeef;
+	}
+	
+	/* 初始化异常发生时自动保存的寄存器 */
+	stack_frame->exception_stack_frame.r0 = (unsigned long)parameter; /* r0 : argument */
+	stack_frame->exception_stack_frame.r1 = 0;   /* r1 */
+	stack_frame->exception_stack_frame.r2 = 0;   /* r2 */
+	stack_frame->exception_stack_frame.r3 = 0;   /* r3 */
+	stack_frame->exception_stack_frame.r12 = 0;   /* r12 */
+	stack_frame->exception_stack_frame.lr = 0;   /* lr 暂时初始化为0 */
+	stack_frame->exception_stack_frame.pc = (unsigned long)tentry;  /* entry */
+	stack_frame->exception_stack_frame.psr = 0x01000000L;    /* PSR */
+	
+	/* 返回线程栈指针 */
+	return stk;
 }
 
 
